50fba30a176a386035355205fc07eeb5c41782e0
Sufiyan  Yusuf <sufiyanyusuf78611@gmail.com>
2019-09-12 12:17:15 +0400

    login poc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Alert} from 'react-bootstrap';
+import './styles/index.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import NavBar from "./components/navbar";
+import { useAuth0 } from "./react-auth0-wrapper";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/profile";
+
+function App() {
+  return (
+    <div className="App">
+      {/* New - use BrowserRouter to provide access to /profile */}
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
